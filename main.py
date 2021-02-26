4c0df9d4f15d6ab4e9b9174f58313f0165b78ed0
shelly schwartz <31975752+shellyganga@users.noreply.github.com>
2021-02-26 00:24:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import discord
 import os
 import secret
+import re
 
 client = discord.Client()
 
 banned_words = [
     "hash", "map", "hashmap", "h4sh", "m4p", "h@sh", "m@p"
 ]
+
 
 @client.event
 async def on_ready():
@@ -16,7 +18,10 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
+    x = re.search("h*sh", message)
+    if(x):
+        await message.channel.send("no. no more fking bad words. get out")
+        return
     for word in banned_words:
         if word in message.content.lower():
             await message.delete()
