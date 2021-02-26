f4e45a182a623e04ca7be0a41fb8f893bccdddf4
vvvuPurdue <vvvu@purdue.edu>
2021-02-26 01:22:55 -0500

    change status and send message when bot connect

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import os
 import markovify
 import secret
 
-client = discord.Client()
-
+# ban the use of hash
 banned_words = [
     "hash", "map", "hashmap", "h4sh", "m4p", "h@sh", "m@p"
 ]
 
+# markvofiy stuff to generate text
+# TODO: fking move this stuff to another goshdamn file, cuz shit lookin messy
 with open("data/insults.txt", "r", encoding="utf-8") as f:
     text = f.read()
 insult_model = markovify.NewlineText(text)
@@ -19,9 +20,16 @@ with open("data/pickuplines.txt", "r", encoding="utf-8") as f:
 pickup_model = markovify.NewlineText(text)
 pickup_model.compile()
 
+# discord bot stuff
+client = discord.Client()
+general_channel = 806599630666203137
+
 @client.event
 async def on_ready():
     print("{0.user} has joined the server and ready to roll!".format(client))
+    await client.get_channel(general_channel).send("hey yall, i'm fking here now")
+    await client.change_presence(activity=discord.Game(name="your mom"))
+
 
 @client.event
 async def on_message(message):
