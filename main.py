87dac81f93e0e9f911ebab9d73d0a51f3d2ddf31
vvvuPurdue <vvvu@purdue.edu>
2021-02-26 00:44:53 -0500

    added insult generator, pickupline generator, and requirements.txt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import markovify
 import secret
 
 client = discord.Client()
@@ -7,6 +8,16 @@ client = discord.Client()
 banned_words = [
     "hash", "map", "hashmap", "h4sh", "m4p", "h@sh", "m@p"
 ]
+
+with open("data/insults.txt", "r", encoding="utf-8") as f:
+    text = f.read()
+insult_model = markovify.NewlineText(text)
+insult_model.compile()
+
+with open("data/pickuplines.txt", "r", encoding="utf-8") as f:
+    text = f.read()
+pickup_model = markovify.NewlineText(text)
+pickup_model.compile()
 
 @client.event
 async def on_ready():
@@ -22,5 +33,13 @@ async def on_message(message):
             await message.delete()
             await message.channel.send("no. no more fking bad words. get out")
             return
+
+    if message.content == "!insult":
+        await message.channel.send(insult_model.make_sentence())
+        return
+
+    if message.content == "!pickup":
+        await message.channel.send(pickup_model.make_sentence())
+        return
 
 client.run(secret.secret)
